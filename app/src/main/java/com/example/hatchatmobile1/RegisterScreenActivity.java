ede59d681b03313e1968ae278fe13a747056c042
Yuval Arbel <Arbelyuval56@gmail.com>
2023-06-13 03:23:50 +0300

    added validation the the password goodnight

--- app/src/main/java/com/example/hatchatmobile1/RegisterScreenActivity.java
+++ app/src/main/java/com/example/hatchatmobile1/RegisterScreenActivity.java
@@ -1,6 +1,9 @@
 package com.example.hatchatmobile1;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,17 +12,12 @@ import androidx.appcompat.widget.TooltipCompat;
 import com.example.hatchatmobile1.databinding.ActivityRegisterScreenBinding;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterScreenActivity extends AppCompatActivity {
     private ActivityRegisterScreenBinding binding;
-    // Regex patterns for password validation
-    private String[] passwordRegexPatterns = {
-            "^.{8,}$",  // At least 8 characters long
-            "^(?=.*[A-Z]).+$",  // At least 1 uppercase letter A - Z
-            "^(?=.*[a-z]).+$",  // At least 1 lowercase letter a - z
-            "^(?=.*\\d).+$",  // At least 1 number 0 - 9
-            "^(?=.*[@$!%*?&]).+$"  // At least 1 special character (!, @, #, $, etc.)
-    };
+    private TextInputEditText passwordInputText;
+    private TextInputLayout passwordInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +26,8 @@ public class RegisterScreenActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        TextInputEditText passwordInputText = binding.passwordInputText;
+        passwordInputText = binding.passwordInputText;
+        passwordInputLayout = binding.passwordInputLayout;
         View passwordRequirementsButton = binding.passwordRequirementsButton;
 
         // Add tooltip to the password requirements button
@@ -36,6 +35,23 @@ public class RegisterScreenActivity extends AppCompatActivity {
 
         // Add click listener to show password requirements dialog
         passwordRequirementsButton.setOnClickListener(v -> showPasswordRequirementsDialog());
+
+        // Add text change listener to the password input field
+        passwordInputText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not needed in this case
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                validatePassword(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
 
     private void showPasswordRequirementsDialog() {
@@ -47,7 +63,36 @@ public class RegisterScreenActivity extends AppCompatActivity {
                 .show();
     }
 
+    private void validatePassword(String password) {
+        boolean isPasswordValid = checkPasswordRequirements(password);
 
+        if (isPasswordValid) {
+            passwordInputLayout.setErrorEnabled(false);
+            passwordInputLayout.setError(null);
+            passwordInputLayout.setEndIconCheckable(true);
+            passwordInputLayout.setEndIconDrawable(R.drawable.ic_checkmark);
+            passwordInputLayout.setEndIconTintList(ColorStateList.valueOf(getResources().getColor(R.color.green)));
+        } else {
+            passwordInputLayout.setError("Password requirements not met!");
+            passwordInputLayout.setEndIconCheckable(false);
+            passwordInputLayout.setEndIconDrawable(null);
+            passwordInputLayout.setEndIconTintList(null);
+        }
+    }
+
+
+    private boolean checkPasswordRequirements(String password) {
+        // Add your password validation logic here using the provided regex patterns
+        String regex1 = "^.{8,}$";
+        String regex2 = "^(?=.*[A-Z]).+$";
+        String regex3 = "^(?=.*[a-z]).+$";
+        String regex4 = "^(?=.*\\d).+$";
+        String regex5 = "^(?=.*[@$!%*?&]).+$";
+
+        return password.matches(regex1) &&
+                password.matches(regex2) &&
+                password.matches(regex3) &&
+                password.matches(regex4) &&
+                password.matches(regex5);
+    }
 }
-
-
