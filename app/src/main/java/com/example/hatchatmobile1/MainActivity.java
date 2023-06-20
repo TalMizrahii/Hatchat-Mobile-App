cd9ed6fb8665a847cc57fc274cf9bf031eb4be62
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2023-06-20 14:57:52 +0300

    Creating the dao for the settings.

--- app/src/main/java/com/example/hatchatmobile1/MainActivity.java
+++ app/src/main/java/com/example/hatchatmobile1/MainActivity.java
@@ -40,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
                         int duration = Toast.LENGTH_SHORT;
                         Toast toast = Toast.makeText(MainActivity.this, text, duration);
                         toast.show();
-
                         // Valid token, start ContactListActivity
                         Intent contactListIntent = new Intent(MainActivity.this, ContactListActivity.class);
                         contactListIntent.putExtra("username", username);
